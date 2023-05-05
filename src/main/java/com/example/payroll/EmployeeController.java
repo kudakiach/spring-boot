37896ff55ce8141ed7faf6a6cec848209eefe67d
kudakiach <kudakiach@gmail.com>
2023-05-05 14:59:31 +0300

    created a Spring MVC REST controller

--- src/main/java/com/example/payroll/EmployeeController.java
+++ src/main/java/com/example/payroll/EmployeeController.java
@@ -1,7 +1,10 @@
 package com.example.payroll;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,24 +12,48 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
+@RestController
 public class EmployeeController {
 
 	private final EmployeeResipotory res;
 	
-	EmployeeController(EmployeeResipotory res) {
+	private final EmployeeModelAssembler assembler;
+	
+	EmployeeController(EmployeeResipotory res, EmployeeModelAssembler assembler) {
 		this.res = res;
+		this.assembler = assembler;
 	}
+	
+	
 	
 	
 //	Aggregate root
 	// tag::get-aggregate
 	@GetMapping("/employees")
-	List<Employee> all() {
-		return res.findAll();
+	CollectionModel<EntityModel<Employee>> all() {
+		List<EntityModel<Employee>> employees = res.findAll().stream()
+				.map(assembler::toModel)
+				.collect(Collectors.toList());
+
+			return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
 	}
 	
+
 	
+
+
+//    The return type of the method has changed from Employee to EntityModel<Employee>. 
+	 			// EntityModel<T> is a generic container from Spring HATEOAS that includes not only the data but a collection of links.
+//
+//    linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel() 
+				// asks that Spring HATEOAS build a link to the EmployeeController 's one() method, and flag it as a self link.
+//
+//    linkTo(methodOn(EmployeeController.class).all()).withRel("employees") 
+				// asks Spring HATEOAS to build a link to the aggregate root, all(), and call it "employees".
+
+
 	
 	//new Employee
 	
@@ -36,12 +63,15 @@ public class EmployeeController {
 	}
 	
 //	Single item
-	
 	@GetMapping("/employees/{id}")
-	Employee one(@PathVariable Long id) {
-		return res.findById(id)
-				.orElseThrow(() -> new EmployeeNotFoundException(id));
+	EntityModel<Employee> one(@PathVariable Long id) {
+
+	  Employee employee = res.findById(id) //
+	      .orElseThrow(() -> new EmployeeNotFoundException(id));
+
+	  return assembler.toModel(employee);
 	}
+	
 	
 	@PutMapping("/employees/{id}")
 	Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
